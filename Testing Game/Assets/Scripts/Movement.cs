f9580092b4de56270fa5e2044322925784f0ca2d
Taylor McKenzie <xx20twinkiie14xx@gmail.com>
2020-07-23 03:57:45 -0700

    First Push
    Push please

---
+++ Testing Game/Assets/Scripts/Movement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Transform tf;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //WHEN USER PRESSES SHIFT, MOVE WITHOUT SPEED AT SINGULAR UNITS
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                tf.position = tf.position + Vector3.up;
+            }
+
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                tf.position = tf.position + Vector3.left;
+            }
+
+            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                tf.position = tf.position + Vector3.down;
+            }
+
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                tf.position = tf.position + Vector3.right;
+            }
+        }
+
+        //WITHOUT SHIFT, THE PLAYER MOVES AS USUAL
+        else
+        {
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                tf.position = tf.position + Vector3.up * speed;
+            }
+
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                tf.position = tf.position + Vector3.left * speed;
+            }
+
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                tf.position = tf.position + Vector3.down * speed;
+            }
+
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                tf.position = tf.position + Vector3.right * speed;
+            }
+        }
+    }
+}
---
+++ Testing Game/Assets/Scripts/NewMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    private Transform tf;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            tf.position = tf.position + Vector3.left * speed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            tf.position = tf.position + Vector3.right * speed;
+        }
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
+        {
+            if (FloorManager.jumps != 0)
+            {
+                rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Force);
+                FloorManager.jumps -= 1;
+                GetComponent<AudioSource>().Play();
+            }
+        }
+    }
+}
